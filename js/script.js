d3bf44ca934d18123f8e8e3427dd2693d1d1bc2d
Ira <irarad1994@gmail.com>
2020-07-18 15:50:13 +0300

    created

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let sum = 0;
+let N = Math.floor(+prompt("Введіть перше число"));
+while(isNaN(N)){
+    N =  +prompt("Введіть перше число");
+}
+
+let M = Math.floor(+prompt("Введіть друге число"));
+
+while(isNaN(M) || N >= M){
+    M = +prompt("Введіть друге число, більше від попереднього");
+}
+
+
+let skipEvenNumbers = confirm ("Пропускати парні числа?");
+ 
+while (N <= M ){
+if(skipEvenNumbers){
+    if(N % 2 ===0){
+        N ++;
+        continue;
+    }
+}
+sum += N;
+N ++;
+}
+
+document.writeln(sum);
+
